98b6fccb08d9fb44ac151f2e90d40748526f2f2d
oomsakkie <42586863+oomsakkie@users.noreply.github.com>
2021-09-22 09:56:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consolePrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mr. Francis");
+        }
+    }
+}
